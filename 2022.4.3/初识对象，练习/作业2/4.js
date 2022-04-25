ab430ae0deb376278cf14843660c32a1547e931c
uesrname-sue <3072496960@qq.com>
2022-04-25 15:19:22 +0800

    js 初识对象

---
+++ 2022.4.3/初识对象，练习/作业2/4.js
@@ -0,0 +1,19 @@
+/**
+ * 打印一个对象的某个属性
+ * @param {object} obj 要打印的对象
+ * @param {string} key 要打印的对象属性名
+ */
+function printProperty(obj, key) {
+  // 完成该函数
+  console.log(obj[key])
+}
+
+var user = {
+  name: 'monica',
+  age: 17,
+  sex: '女',
+};
+
+printProperty(user, 'name'); // 打印：monica
+printProperty(user, 'age'); // 打印：17
+printProperty(user, 'sex'); // 打印：女
